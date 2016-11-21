63fa258bb24fe2dd2921de370deb956d82da5ea5
Cezar Luiz <cezarluiz.c@gmail.com>
2016-11-21 03:17:35 -0200

    React and redux installed and some components are already created

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,80 +1,106 @@
 // TODO: Implement vuejs, bind event on buttons
-
-let io = require('socket.io-client');
-
-let socket = io.connect('http://localhost:8888');
 let $ = document.querySelector.bind(document);
 
+let React = require('react');
+let ReactDOM = require('react-dom');
+
+let io = require('socket.io-client');
+let socket = io.connect('http://localhost:8888');
+
 // Components
-let loginForm = $('#login-form');
-let play = $('#play');
-let playButton = $('#play button');
-let waitingOponent = $('#waiting-oponent');
-let field = $('#field');
-let cardsContainer = $('#cards');
+let app = $('#app');
 
-let playerData = {};
 
-$('#login-form').addEventListener('submit', e => {
-  e.preventDefault();
+const PlayButton = ({user}) => {
+  console.log(user);
+  const play = () => socket.emit('play', user);
 
-  playerData = {
-    nickname: $('#nickname').value,
-    password: $('#password').value,
-    firstName: $('#first-name').value,
-    lastName: $('#last-name').value,
-    connected: true
+  return (
+    <div id="play">
+      <button onClick={play}>Play!</button>
+    </div>
+  );
+};
+
+PlayButton.propTypes = {
+  battle: React.PropTypes.object
+};
+
+const WaitingOponent = () => {
+  return (
+    <div id="waiting-oponent">
+      <p>Waiting a oponent!</p>
+      <button id="cancel">Cancel</button>
+    </div>
+  );
+};
+
+const Field = ({battle}) => {
+  return (
+    <div id="field">
+      <div id="cards"></div>
+    </div>
+  );
+};
+
+Field.propTypes = {
+  battle: React.PropTypes.object.isRequired
+};
+
+const LoginForm = () => {
+  let playerData = {};
+
+  const login = e => {
+    e.preventDefault();
+
+    playerData = {
+      nickname: $('#nickname').value,
+      password: $('#password').value,
+      firstName: $('#first-name').value,
+      lastName: $('#last-name').value,
+      connected: true
+    };
+
+    socket.emit('login', playerData);
   };
 
-  socket.emit('login', playerData);
-});
+  return (
+    <form id="login-form" onSubmit={login}>
+      <input type="text" id="nickname" placeholder="Nickname" />
+      <input type="text" id="password" placeholder="Password" defaultValue="sdaddasd" />
+      <input type="text" id="first-name" placeholder="First name" defaultValue="sdaddasd" />
+      <input type="text" id="last-name" placeholder="Last name" defaultValue="sdaddasd" />
+
+      <button id="login-form">Enter!</button>
+    </form>
+  );
+};
 
 socket.on('logged', function (user) {
   // User was logged success
-  loginForm.classList.add('hidden');
-  play.classList.remove('hidden');
+  ReactDOM.render(<PlayButton user={user} />, app);
+});
 
-  playButton.addEventListener('click', () => {
-    socket.emit('play', user);
-  });
+socket.on('waiting a oponent', function () {
+  ReactDOM.render(<WaitingOponent />, app);
+});
 
-  socket.on('waiting a oponent', function () {
-    play.classList.add('hidden');
-    waitingOponent.classList.remove('hidden');
-  });
+socket.on('battle starts', function (battle) {
+  console.log('battle starts', battle);
 
-  socket.on('battle starts', function (battle) {
-    console.log('battle starts', battle);
+  ReactDOM.render(<Field battle={battle} />, app);
 
-    play.classList.add('hidden');
-    waitingOponent.classList.add('hidden');
-    field.classList.remove('hidden');
+  socket.on('battle updated', function (data) {
+    let {payload, type} = data;
+    let {currentPlayer, battle} = payload;
+    let {player} = currentPlayer;
 
-    renderCards(battle.players[playerData.nickname].player.cardsOnHand);
-
-    socket.on('battle updated', function (data) {
-      let {payload, type} = data;
-      let {currentPlayer, battle} = payload;
-      let {player} = currentPlayer;
-
-      switch (type) {
-        case 'DRAW_CARD_ON_FIELD':
-          console.log(`The player ${player.nickname} draw the card ${payload.cardDrawed.card.name}`);
-          break;
-      }
-    });
+    switch (type) {
+      case 'DRAW_CARD_ON_FIELD':
+        console.log(`The player ${player.nickname} draw the card ${payload.cardDrawed.card.name}`);
+        break;
+    }
   });
 });
 
-function renderCards(cards) {
-  cardsContainer.innerHTML = '';
-
-  cards.forEach(card => {
-    let btn = document.createElement('button');
-    btn.innerText = card.card.name;
-
-    cardsContainer.appendChild(btn);
-  });
-
-  field.appendChild(cardsContainer);
-}
+ReactDOM.render(<LoginForm />, app);
--- webpack.dev.js
+++ webpack.dev.js
@@ -18,6 +18,18 @@ module.exports = {
     open: true,
     contentBase: path.join(__dirname, 'dist')
   },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
   plugins: [
     new CopyWebpackPlugin([
       {
